38401af89893398f03466fcb0d75f6014d638074
rkartha <rohitkartha9@gmail.com>
2024-01-02 00:39:54 -0600

    show object, gitignore db

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 import httpx
 from bs4 import BeautifulSoup
+import sqlite3
+
+conn  = sqlite3.connect('us_mic_data.db')
 
 class State:
   def __init__(self, name, cities, link):
@@ -9,20 +12,25 @@ class State:
 
 
 class City:
-  def __init__(self, name, link):
+  def __init__(self, name, link, shows):
     self.name = name
     self.link = link
-   
+    self.shows = shows
 
-r = httpx.get('https://badslava.com/open-mics-state.php?state=NC&type=Comedy')
-soup = BeautifulSoup(r.text, 'html.parser')
-print(len(list(soup.find_all("td"))))
 
-for i in range(1, 406, 14):  # Starting from 1, increment by 8 until less than 50
-    print(list(soup.find_all("td")[i]))
+class Show:
+  def __init__(self, name, time, venue, address, freq, cost, email, link, phone, hostname):
+    self.name = name
+    self.link = link
+    self.time = time
+    self.venue = venue
+    self.address = address
+    self.freq = freq
+    self.cost = cost
+    self.email = email
+    self.phone = phone
+    self.hostname = hostname
 
-#print(len(list(soup.children)))
-    
 main_page = httpx.get('https://badslava.com/')
 soup = BeautifulSoup(main_page.text, 'html.parser')
 united_states_geoblock = (soup.find('div', class_='united-states'))
@@ -31,7 +39,8 @@ state_geounits = list(united_states_geoblock.find_all('div', class_='geoUnit'))
 
 all_states_data = []
 
-for i in range(0, len(state_geounits)):
+#len(state_geounits)
+for i in range(0, 1):
     curr_cities = [] 
     curr_state_soup = state_geounits[i]
     state_name = curr_state_soup.find('h3').find('a').text
@@ -39,7 +48,29 @@ for i in range(0, len(state_geounits)):
     
     soup_cities = list(curr_state_soup.find_all('li'))
     for soup_city in soup_cities:
-        curr_city = City(soup_city.find('a').text, "https://badslava.com/" + soup_city.find('a').get('href'))
+        curr_city_req = httpx.get("https://badslava.com/" + soup_city.find('a').get('href'))
+        curr_city_soup= BeautifulSoup(curr_city_req.text, 'html.parser')
+        curr_cities_shows = []
+
+        shows_soup = list(curr_city_soup.find_all("td"))
+        for i in range(1, len(shows_soup), 14):
+            show_name = shows_soup[i].text 
+            show_time = shows_soup[i-1].text 
+            show_venue = shows_soup[i+1].text 
+            show_address = shows_soup[i+2].text
+            show_freq = shows_soup[i+6].text
+            show_cost = shows_soup[i+7].text
+            show_email = shows_soup[i+9].find('a').get('href')
+            show_hostname = shows_soup[i+9].text
+            show_website =  shows_soup[i+10].find('a').get('href')
+            show_phone = shows_soup[i+11].text
+
+            curr_show = Show(show_name, show_time, show_venue, show_address, show_freq, 
+                             show_cost, show_email, show_website, show_phone, show_hostname )
+            
+            curr_cities_shows.append(curr_show)
+
+        curr_city = City(soup_city.find('a').text, "https://badslava.com/" + soup_city.find('a').get('href'), curr_cities_shows)
         curr_cities.append(curr_city)
     curr_state = State(state_name, curr_cities, state_link)
     all_states_data.append(curr_state)
